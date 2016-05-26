cfa9ac225dde690305e77ecc2f45747123d05411
shangfeiSF <tony20100125@126.com>
2016-05-26 16:32:52 +0800

    new panel with tabs

--- main/injections/scripts/modules/panel.js
+++ main/injections/scripts/modules/panel.js
@@ -1,317 +1,134 @@
 function Panel(fairy) {
-  this.nodes = {
-    /* init add */
-    panel: null,
-    trigger: null,
-    container: null,
-
-    /* render add */
-    root: null,
-
-    count: null,
-    specs: null,
-
-    purchase: null,
-    add2MyCart: null,
-    showMycart: null
+  this.ids = {
+    panel: '_panel_',
+    trigger: '_trigger_',
+    tabs: '_tabs_',
+    container: '_container_'
   }
 
-  this._origScriptSessionIdPattern = /dwr\.engine\.\_origScriptSessionId\s*\=\s*\"(.*)\"/
+  this.nodes = {
+    panel: null,
+    trigger: null,
+    tabs: null,
+    container: null,
+
+    tabBlocks: {},
+    tabBlockTriggers: {}
+  }
+
+  this.tabConfig = {
+    tabBlock: 'tabBlock',
+    tabBlockTrigger: 'tabBlockTrigger',
+
+    selectedTabBlockTrigger: 'selectedTabBlockTrigger',
+
+    tabBlocks: {},
+    tabBlockTriggers: {},
+  }
 
   this.fairy = fairy
+
+  this.boot()
+  this.init()
 }
 
 Stamp.$.extend(Panel.prototype, {
-  init: function () {
+  boot: function () {
     var self = this
 
-    var panelId = '_panel_'
-    var triggerId = '_trigger_'
+    var tabConfig = self.tabConfig
+    var layout = self.fairy.layout
 
-    var panel = Stamp.$('<div></div>').attr('id', panelId)
-    var trigger = Stamp.$('<div></div>').attr('id', triggerId).text('快速下单')
-    var container = Stamp.$('<div></div>').attr('id', '_container_')
+    var blockSuffix = tabConfig.tabBlock.replace(tabConfig.tabBlock[0], tabConfig.tabBlock[0].toUpperCase())
+    var triggerSuffix = tabConfig.tabBlockTrigger.replace(tabConfig.tabBlockTrigger[0], tabConfig.tabBlockTrigger[0].toUpperCase())
 
-    self.nodes.panel = panel
-    self.nodes.trigger = trigger
-    self.nodes.container = container
+    Stamp.$.each(layout, function (prop, config) {
+      tabConfig.tabBlocks[config.anchor] = {
+        klass: [tabConfig.tabBlock, config.anchor + blockSuffix].join(' '),
+        default: config.default ? true : false
+      }
 
-    panel.append(trigger)
-    panel.append(container)
-    Stamp.$('body').append(panel)
+      tabConfig.tabBlockTriggers[config.anchor] = {
+        klass: [tabConfig.tabBlockTrigger, config.anchor + triggerSuffix].join(' '),
+        text: config.triggerText,
+        default: config.default ? true : false
+      }
+    })
+  },
 
-    new Draggable(panelId, {
-      handle: triggerId
+  init: function () {
+    var self = this
+    var nodes = self.nodes
+
+    Stamp.$.each(self.ids, function (name, id) {
+      nodes[name] = Stamp.$('<div></div>').attr('id', id)
     })
 
+    nodes.panel.css('font-size', '20px')
+    nodes.panel.append(nodes.trigger.text('快速下单'))
+    nodes.panel.append(nodes.tabs)
+    nodes.panel.append(nodes.container)
+
     self.render()
+    self.bind()
+
+    self.append()
   },
 
   render: function () {
     var self = this
 
-    self.countRender()
-    self.specsRender()
-    self.purchaseRender()
-    self.add2MyCartRender()
-    self.showMyCartRender()
-
-    self.countBind()
-    self.specsBind()
-    self.purchaseBind()
-    self.add2MyCartBind()
-    self.showMyCartBind()
-
-    self.append()
-  },
-
-  countRender: function () {
-    var self = this
-
-    var count = Stamp.$('<input>', {
-      type: 'number',
-      id: '_count_',
-      min: 1,
-      value: 1
-    }).css({
-      width: '4em',
-      margin: '0 0.5em 0 0',
-      'text-aligen': 'center'
-    })
-
-    self.nodes.count = count
-  },
-
-  specsRender: function () {
-    var self = this
-
-    var cache = self.fairy.cache
-    var details = self.fairy.details
-
-    var count = self.nodes.count
-
-    var specs = Stamp.$('<div class="specs"></div>')
-
-    Stamp.$.each(details.goodsAttrList, function (index, attr) {
-      var wrap = Stamp.$('<sapn class="spec"></sapn>')
-
-      var id = ['_sepc_', index].join('')
-
-      var spec = Stamp.$('<input>', {
-        type: 'radio',
-        id: id,
-        name: 'spec',
-        value: index,
-      }).data('info', attr)
-
-      var label = Stamp.$('<label>', {
-        for: id
-      }).text(attr.attrName)
-
-      if (index == 0) {
-        spec.attr('checked', 'checked')
-        label.addClass('selected')
-
-        count.attr('max', attr.buyLimit)
-
-        cache.specIndex = index
-        cache.buyLimit = attr.buyLimit
-
-        var limit = ['(购买数量上限：', attr.buyLimit, ')'].join('')
-        count.after(Stamp.$('<span>').css({
-          color: '#aaa'
-        }).text(limit))
-      }
-
-      wrap.append(spec).append(label)
-
-      specs.append(wrap)
-    })
-    
-    self.nodes.specs = specs
-  },
-
-  purchaseRender: function () {
-    var self = this
-
-    var purchase = Stamp.$('<input>', {
-      type: 'button',
-      id: '_purchase_',
-      value: '立即购买'
-    }).addClass('btn btn-success')
-
-    self.nodes.purchase = purchase
-  },
-
-  add2MyCartRender: function () {
-    var self = this
-
-    var add2MyCart = Stamp.$('<input>', {
-      type: 'button',
-      id: '_add2MyCart_',
-      value: '加入购物车'
-    }).addClass('btn btn-warning')
-
-    self.nodes.add2MyCart = add2MyCart
-  },
-
-  showMyCartRender: function () {
-    var self = this
-
-    var showMycart = Stamp.$('<input>', {
-      type: 'button',
-      id: '_showMycart_',
-      value: '我的购物车'
-    }).addClass('btn btn-info')
-
-    self.nodes.showMycart = showMycart
-  },
-
-  countBind: function () {
-    var self = this
-
-    self.nodes.count.on('change', function () {
-      var target = Stamp.$(this)
-      var max = +target.attr('max')
-
-      Number(target.val()) > max && target.val(String(max))
-    })
-  },
-
-  specsBind: function () {
-    var self = this
-
-    var cache = self.fairy.cache
-
     var nodes = self.nodes
+    var tabConfig = self.tabConfig
 
-    nodes.specs.on('change', function (e) {
+    var tabs = nodes.tabs
+    var container = nodes.container
+
+    Stamp.$.each(tabConfig.tabBlocks, function (prop, config) {
+      var block = Stamp.$('<div>', {class: config.klass})
+
+      nodes.tabBlocks[prop] = block
+
+      config.default ? block.show() : block.hide()
+
+      container.append(block)
+    })
+
+    Stamp.$.each(tabConfig.tabBlockTriggers, function (prop, config) {
+      var trigger = Stamp.$('<div>', {class: config.klass})
+        .text(config.text)
+        .attr('data-show', tabConfig.tabBlocks[prop].klass)
+
+      nodes.tabBlockTriggers[prop] = trigger
+
+      config.default && trigger.addClass(self.tabConfig.selectedTabBlockTrigger)
+
+      tabs.append(trigger)
+    })
+  },
+
+  bind: function () {
+    var self = this
+
+    var tabConfig = self.tabConfig
+    var selectedTabBlockTrigger = self.tabConfig.selectedTabBlockTrigger
+
+    var tabs = self.nodes.tabs
+    var container = self.nodes.container
+
+    tabs.delegate('.' + tabConfig.tabBlockTrigger, 'click', function (e) {
       var target = Stamp.$(e.target)
+      var show = target.attr('data-show')
 
-      Stamp.$.each(Stamp.$(this).find('label'), function (index, node) {
-        var node = Stamp.$(node)
-
-        node.removeClass('selected')
-
-        node.attr('for').split('_').pop() === target.val() && node.addClass('selected')
+      Stamp.$.each(tabs.children(), function (index, child) {
+        var child = Stamp.$(child)
+        child.removeClass(selectedTabBlockTrigger)
       })
+      target.addClass(selectedTabBlockTrigger)
 
-      nodes.count.attr('max', Number(target.data('info').buyLimit))
-
-      cache.specIndex = target.val()
-      cache.buyLimit = target.data('spec').buyLimit
-    })
-  },
-
-  purchaseBind: function () {
-    var self = this
-
-    var cache = self.fairy.cache
-    var details = self.fairy.details
-
-    var nodes = self.nodes
-    var purchase = nodes.purchase
-    var count = nodes.count
-
-    purchase.on('click', function () {
-      var params = {
-        'buyGoodsNowBean.goods_id': self.fairy.cache.goodsId,
-        'buy_type': details.goodsStatus.lottery ? '3' : '2',
-        'buyGoodsNowBean.goods_attr_id': details.goodsAttrList[cache.specIndex].id,
-        'buyGoodsNowBean.goods_num': count.val(),
-        'goodsTicketAttr': details.goodsAttrList[cache.specIndex].id
-      }
-
-      self.fairy.loader.post('buy', params)
-        .then(function (data) {
-          if (data.result.search('date_form') > -1 && data.result.search('gwc gwc2') > -1) {
-            purchase.off()
-
-            nodes.root.hide()
-            count.attr('disabled', 'disabled').hide()
-
-            cache.html4order = data.result
-            cache.count = count.val()
-
-            var needVerify = data.result.search('手机确认') > -1 ? true : false
-            self.fairy.loader.init(needVerify)
-          }
-        })
-    })
-  },
-
-  add2MyCartBind: function () {
-    var self = this
-
-    var cache = self.fairy.cache
-    var details = self.fairy.details
-
-    var nodes = self.nodes
-    var count = nodes.count
-    var add2MyCart = nodes.add2MyCart
-
-    add2MyCart.on('click', function () {
-      self.fairy.loader.post('user')
-        .asCallback(function (error, data) {
-          if (data.textStatus === 'success') {
-            cache.userType = data.result.userType
-            cache.userId = data.result.userId
-
-            var mock = {
-              _origScriptSessionId: self._get_origScriptSessionId()
-            }
-
-            ShoppingCartAction.addGoodsToShoppingCartLS(details.goodsId, count.val(), details.goodsAttrList[cache.specIndex].id, function (msg) {
-              self.fairy.cart.addRecordsRender(msg)
-            }, mock)
-          }
-        })
-    })
-  },
-
-  _get_origScriptSessionId: function () {
-    var self = this
-
-    var _origScriptSessionId = undefined
-
-    Stamp.$.ajax({
-      type: 'GET',
-      url: 'http://jiyou.biz.11185.cn/dwr/engine.js',
-      cache: false,
-      async: false,
-      success: function (content) {
-        var matches = self._origScriptSessionIdPattern.exec(content)
-        matches && matches.length == 2 && ( _origScriptSessionId = matches.pop() + Math.floor(Math.random() * 31793))
-      },
-      error: function () {
-        _origScriptSessionId = undefined
-      }
-    })
-
-    return _origScriptSessionId
-  },
-
-  showMyCartBind: function () {
-    var self = this
-
-    var cache = self.fairy.cache
-
-    var nodes = self.nodes
-    var showMycart = nodes.showMycart
-
-    showMycart.on('click', function () {
-      Stamp.$.ajax({
-        tupe: 'GET',
-        url: 'http://jiyou.biz.11185.cn/u/show.html',
-        success: function (html) {
-          cache.html4cart = html
-          self.fairy.cart.render()
-        },
-        error: function () {
-          cache.html4cart = ''
-          // TODO: 拉取购物车失败
-        },
-        dataType: 'html'
+      Stamp.$.each(container.children(), function (index, child) {
+        var child = Stamp.$(child)
+        child.hasClass(show) ? child.show() : child.hide()
       })
     })
   },
@@ -320,36 +137,12 @@ Stamp.$.extend(Panel.prototype, {
     var self = this
     var nodes = self.nodes
 
-    var sections = [
-      'specsSection',
-      'countSection',
-      'purchaseSection',
-      'add2MyCartSection',
-      'showMyCartSection'
-    ]
+    Stamp.$('body').append(nodes.panel)
 
-    sections = Stamp.$.map(sections, function (klass) {
-      return Stamp.$('<div>', {
-        class: ['section', klass].join(' ')
-      })
+    new Draggable(self.ids.panel, {
+      handle: self.ids.trigger
     })
-
-    sections[0].append(nodes.specs)
-    sections[1].append(nodes.count)
-    sections[2].append(nodes.purchase)
-    sections[3].append(nodes.add2MyCart)
-    sections[4].append(nodes.showMycart)
-
-    var root = Stamp.$('<div class="goodsRoot"></div>')
-    Stamp.$.each(sections, function (index, section) {
-      root.append(section)
-    })
-
-    root.prepend(Stamp.$('<span class="tip">').text('提示：选择规格和数量后生成快速订单'))
-
-    nodes.root = root
-    nodes.container.append(root)
-  },
+  }
 })
 
 module.exports = Panel
